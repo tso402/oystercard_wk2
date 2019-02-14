871ddd92b786c53add7ad41948ad8cb95dd57163
Steve Kahn <steve@kahn.org.uk>
2019-02-14 16:22:02 +0000

    Step 16 added tests for fares to equate to zones

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -31,4 +31,45 @@ describe Journey do
     journey = Journey.new()
     expect(journey.fare).to eq Journey::PENALTY_FARE
   end
+
+  context "Charges a fare for going between stations" do
+    let(:station) {double :station}
+    let(:other_station) {double :other_station}
+    it "of 1 for zone 1 to zone 1" do
+      zones(1,1)
+      trip = Journey.new(station)
+      trip.end(other_station)
+      expect(trip.fare).to eq 1
+    end
+    it "of 2 for zone 2 to zone 1" do
+      zones(2,1)
+      trip = Journey.new(station)
+      trip.end(other_station)
+      expect(trip.fare).to eq 2
+    end
+    it "of 3 for zone 5 to zone 2" do
+      zones(5,2)
+      trip = Journey.new(station)
+      trip.end(other_station)
+      expect(trip.fare).to eq 3
+    end
+    it "of 4 for zone 2 to zone 6" do
+      zones(2,6)
+      trip = Journey.new(station)
+      trip.end(other_station)
+      expect(trip.fare).to eq 4
+    end
+    it "of 1 for zone 2 to zone 2" do
+      zones(2,2)
+      trip = Journey.new(station)
+      trip.end(other_station)
+      expect(trip.fare).to eq 2
+    end
+
+    def zones(entry, exit)
+      allow(station).to receive(:zone).and_return(entry)
+      allow(other_station). to receive(:zone).and_return(exit)
+    end
+  end
+
 end
